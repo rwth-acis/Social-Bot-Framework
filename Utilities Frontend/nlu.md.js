a8dbd4ffa95db292fca30e97b17244b8bd698c93
Niels Wießner <n.h.wiessner@gmail.com>
2019-09-04 12:00:21 +0200

    Add a frontend for training Rasa NLU models and example data

---
+++ Utilities Frontend/nlu.md.js
@@ -0,0 +1,63 @@
+// Based on https://github.com/RasaHQ/rasa/blob/c51bda7c8e32c012f24f80ef19c08121b924a1a3/examples/moodbot/data/nlu.md
+
+var exampleconfig = `## intent:greet
+- hey
+- hello
+- hi
+- hello there
+- good morning
+- good evening
+- moin
+- hey there
+- let's go
+- hey dude
+- goodmorning
+- goodevening
+- good afternoon
+
+## intent:goodbye
+- cu
+- good by
+- cee you later
+- good night
+- good afternoon
+- bye
+- goodbye
+- have a nice day
+- see you around
+- bye bye
+- see you later
+
+## intent:mood_happy
+- perfect
+- very good
+- great
+- amazing
+- feeling like a king
+- wonderful
+- I am feeling very good
+- I am great
+- I am amazing
+- I am going to save the world
+- super
+- extremely good
+- so so perfect
+- so good
+- so perfect
+
+## intent:mood_unhappy
+- my day was horrible
+- I am sad
+- I don't feel very well
+- I am disappointed
+- super sad
+- I'm so sad
+- sad
+- very sad
+- unhappy
+- not so good
+- not very good
+- extremely sad
+- so saad
+- so sad
+`;
