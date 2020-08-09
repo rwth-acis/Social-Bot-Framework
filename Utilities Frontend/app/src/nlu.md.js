273de33f4d746db3009cd96be20c42736b6a4e0e
Aaron David Conrardy <50965489+Aran30@users.noreply.github.com>
2020-08-09 20:40:28 +0200

    Update nlu.md.js

--- Utilities Frontend/app/src/nlu.md.js
+++ Utilities Frontend/app/src/nlu.md.js
@@ -1,63 +1,52 @@
 // Based on https://github.com/RasaHQ/rasa/blob/c51bda7c8e32c012f24f80ef19c08121b924a1a3/examples/moodbot/data/nlu.md
 
-export default `## intent:greet
+export default `## intent:  begrüßung
+
+- Hallo
+
+- Moin
+
+- hallo
+
 - hey
-- hello
-- hi
-- hello there
-- good morning
-- good evening
-- moin
-- hey there
-- let's go
-- hey dude
-- goodmorning
-- goodevening
-- good afternoon
 
-## intent:goodbye
-- cu
-- good by
-- cee you later
-- good night
-- good afternoon
-- bye
-- goodbye
-- have a nice day
-- see you around
-- bye bye
-- see you later
+- Hi
 
-## intent:mood_happy
-- perfect
-- very good
-- great
-- amazing
-- feeling like a king
-- wonderful
-- I am feeling very good
-- I am great
-- I am amazing
-- I am going to save the world
-- super
-- extremely good
-- so so perfect
-- so good
-- so perfect
+- Guten Tag
 
-## intent:mood_unhappy
-- my day was horrible
-- I am sad
-- I don't feel very well
-- I am disappointed
-- super sad
-- I'm so sad
-- sad
-- very sad
-- unhappy
-- not so good
-- not very good
-- extremely sad
-- so saad
-- so sad
+- guten tag 
+
+- hallöchen
+
+## intent: bestätigung
+
+ -yes
+
+- Yes
+
+- Jop
+
+- Ja
+
+- oke
+
+- ja
+
+-jawohl
+
+- ja gerne
+
+- natürlich
+
+## intent: moodle
+
+- ich möchte ein Quiz anfangen
+
+- ich will ein moodle quiz machen
+
+- Ich möchte gern ein Moodle Quiz machen
+
+- Können wir ein Moodle Quiz starten?
+
+- ich will das Moodle Quiz beginnen
 `;
