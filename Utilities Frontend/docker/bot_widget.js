05f4743c4660d2368490a3e2ad693a7fcdad812d
Leogomat <leogomat@gmail.com>
2020-09-10 14:28:58 +0200

    fixed status messages

--- Utilities Frontend/docker/bot_widget.js
+++ Utilities Frontend/docker/bot_widget.js
@@ -1,5 +1,5 @@
-requirejs(['jqueryui', 'lodash', 'lib/yjs-sync', 'canvas_widget/GenerateViewpointModel', 'canvas_widget/EntityManager','promise!Guidancemodel'],
- function ($, _, yjsSync, GenerateViewpointModel, EntityManager, guidance) {
+requirejs(['jqueryui', 'lodash', 'lib/yjs-sync','promise!Guidancemodel'],
+ function ($, _, yjsSync, guidance) {
     $(function () {
         yjsSync().done(function (y, spaceTitle) {
             console.info('DEBUG: Yjs successfully initialized in room ' + spaceTitle + ' with y-user-id: ' + y.db.userId);
@@ -19,7 +19,6 @@ requirejs(['jqueryui', 'lodash', 'lib/yjs-sync', 'canvas_widget/GenerateViewpoin
             y.share.storeName.bindTextarea(storeNameInput);
             y.share.loadName.bindTextarea(loadNameInput);
 
-
             $submitModel.click(function () {
                 var sendStatus = $('#sendStatus');
                 var endpoint = y.share.sbfManager.toString();
@@ -27,14 +26,15 @@ requirejs(['jqueryui', 'lodash', 'lib/yjs-sync', 'canvas_widget/GenerateViewpoin
                 sendStatus.text("Sending...");
                 
                 var xhr = new XMLHttpRequest();
-                xhr.addEventListener("load", () => { 
-                    sendStatus.text("Successfully sent.");
+                xhr.onload = function() {
+                    if (xhr.status == 200) {
+                        sendStatus.text("Successfully sent.");
+                    } else {
+                        sendStatus.text("Sending failed.");
+                    }
                     cleanStatus('sendStatus');
-                });
-                xhr.addEventListener("error", () => {
-                    sendStatus.text("Sending failed.");
-                    cleanStatus('sendStatus');
-                });
+                }
+                
                 xhr.open('POST', endpoint + '/bots');
                 xhr.setRequestHeader("Content-Type", "application/json");
                 xhr.send(JSON.stringify(model));
@@ -46,19 +46,29 @@ requirejs(['jqueryui', 'lodash', 'lib/yjs-sync', 'canvas_widget/GenerateViewpoin
                 var model = y.share.data.get('model');
                 var storeStatus = $('#storeStatus');
                 storeStatus.text("Storing...");
-                
-                var xhr = new XMLHttpRequest();
-                xhr.addEventListener("load", () => {
-                    storeStatus.text("Successfully stored.");
+
+                if (name && model) {
+                    var xhr = new XMLHttpRequest();
+                    xhr.onload = function() {
+                        if (xhr.status == 200) {
+                            storeStatus.text("Successfully stored.");
+                        } else {
+                            storeStatus.text("Storing failed.");
+                        }
+                        cleanStatus('storeStatus');
+                    };
+                    xhr.open('POST', endpoint + '/models/' + name);
+                    xhr.setRequestHeader("Content-Type", "application/json");
+                    xhr.send(JSON.stringify(model)); 
+                } else {
+                    if (!name) {
+                        storeStatus.text("Invalid model name.");
+                    } else {
+                        storeStatus.text("Model is empty.");
+                    }
                     cleanStatus('storeStatus');
-                })
-                xhr.addEventListener("error", () => {
-                    storeStatus.text("Storing failed.");
-                    cleanStatus('storeStatus');
-                });
-                xhr.open('POST', endpoint + '/models/' + name);
-                xhr.setRequestHeader("Content-Type", "application/json");
-                xhr.send(JSON.stringify(model)); 
+                    
+                }
             });
 
             $loadModel.click(function () {
@@ -80,65 +90,71 @@ requirejs(['jqueryui', 'lodash', 'lib/yjs-sync', 'canvas_widget/GenerateViewpoin
                 xhr.responseType = 'json';
                 xhr.onload = function() {
                     var data = xhr.response;
-                    var initAttributes = function(attrs, map){
-                        if(attrs.hasOwnProperty('[attributes]')){
-                            var attr = attrs['[attributes]'].list;
-                            for(var key in attr){
-                                if(attr.hasOwnProperty(key)){
-                                    if(attr[key].hasOwnProperty('key')){
-                                        var ytext = map.set(attr[key].key.id, Y.Text);
-                                        ytext.insert(0, attr[key].key.value);
-                                    }
-                                    else { 
-                                        var ytext = map.set(attr[key].value.id, Y.Text);
-                                        ytext.insert(0, attr[key].value.value);
+                    if (data && name) {
+                        var initAttributes = function(attrs, map){
+                            if(attrs.hasOwnProperty('[attributes]')){
+                                var attr = attrs['[attributes]'].list;
+                                for(var key in attr){
+                                    if(attr.hasOwnProperty(key)){
+                                        if(attr[key].hasOwnProperty('key')){
+                                            var ytext = map.set(attr[key].key.id, Y.Text);
+                                            ytext.insert(0, attr[key].key.value);
+                                        }
+                                        else { 
+                                            var ytext = map.set(attr[key].value.id, Y.Text);
+                                            ytext.insert(0, attr[key].value.value);
+                                        }
                                     }
                                 }
-                            }
-    
-                        }else{
-                            for(var key in attrs){
-                                if(attrs.hasOwnProperty(key)){
-                                    var value = attrs[key].value;
-                                    if(!value.hasOwnProperty('option')){
-                                        if(value.value instanceof String){
-                                            var ytext = map.set(value.id, Y.Text);
-                                            ytext.insert(0, value.value);
+        
+                            }else{
+                                for(var key in attrs){
+                                    if(attrs.hasOwnProperty(key)){
+                                        var value = attrs[key].value;
+                                        if(!value.hasOwnProperty('option')){
+                                            if(value.value instanceof String){
+                                                var ytext = map.set(value.id, Y.Text);
+                                                ytext.insert(0, value.value);
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
-                    }
-                    if(guidance.isGuidanceEditor())
-                        y.share.data.set('guidancemodel', data);
-                    else
-                        y.share.data.set('model', data);
-                    for(var key in data.nodes){
-                        if (data.nodes.hasOwnProperty(key)) {
-                            var entity = data.nodes[key];
-                            var map = y.share.nodes.set(key, Y.Map);
-                            var attrs = entity.attributes;
-                            if(entity.hasOwnProperty('label')){
-                                var ytext = map.set(entity.label.value.id, Y.Text);
-                                ytext.insert(0, entity.label.value.value);
+                        if(guidance.isGuidanceEditor())
+                            y.share.data.set('guidancemodel', data);
+                        else
+                            y.share.data.set('model', data);
+                        for(var key in data.nodes){
+                            if (data.nodes.hasOwnProperty(key)) {
+                                var entity = data.nodes[key];
+                                var map = y.share.nodes.set(key, Y.Map);
+                                var attrs = entity.attributes;
+                                if(entity.hasOwnProperty('label')){
+                                    var ytext = map.set(entity.label.value.id, Y.Text);
+                                    ytext.insert(0, entity.label.value.value);
+                                }
+                                initAttributes(attrs, map);
                             }
-                            initAttributes(attrs, map);
                         }
-                    }
-                    for(var key in data.edges){
-                        if (data.edges.hasOwnProperty(key)) {
-                            var entity = data.edges[key];
-                            var map = y.share.edges.set(key, Y.Map);
-                            var attrs = entity.attributes;
-                            if(entity.hasOwnProperty('label')){
-                                var ytext = map.set(entity.label.value.id, Y.Text);
-                                ytext.insert(0, entity.label.value.value);
+                        for(var key in data.edges){
+                            if (data.edges.hasOwnProperty(key)) {
+                                var entity = data.edges[key];
+                                var map = y.share.edges.set(key, Y.Map);
+                                var attrs = entity.attributes;
+                                if(entity.hasOwnProperty('label')){
+                                    var ytext = map.set(entity.label.value.id, Y.Text);
+                                    ytext.insert(0, entity.label.value.value);
+                                }
+                                initAttributes(attrs, map);
                             }
-                            initAttributes(attrs, map);
                         }
+                        y.share.canvas.set('ReloadWidgetOperation', 'import');
+                    } else {
+                        $(loadStatus).text("Loading failed.");
+                        cleanStatus('loadStatus');
                     }
-                    y.share.canvas.set('ReloadWidgetOperation', 'import');
+                    
                 }
                 xhr.send(null)
             });
