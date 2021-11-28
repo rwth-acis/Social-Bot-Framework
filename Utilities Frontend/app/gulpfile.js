32af1d7639a86eb4b14d76c8928ae612662bb00b
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-11-28 22:42:30 +0100

    use rollup as module bundler

--- Utilities Frontend/app/gulpfile.js
+++ Utilities Frontend/app/gulpfile.js
@@ -33,14 +33,31 @@ gulp.task('copy:src', () => {
     return merge(copySrc, copyIndex);
 });
 
-gulp.task('clean-build:full', gulp.series(
-    'clean:src',
-    'copy:modules',
-    'copy:src'
-));
+gulp.task(
+  "clean-build:full",
+  gulp.series("clean:src", "copy:modules", "copy:src", build)
+);
 
-gulp.task('clean-build:src', gulp.series(
-    'clean:src',
-    'copy:src'
-));
 
+async function build() {
+  loadConfigFile(path.resolve(__dirname, "rollup.config.js"), {
+    format: "es",
+  }).then(async ({ options, warnings }) => {
+    // "warnings" wraps the default `onwarn` handler passed by the CLI.
+    // This prints all warnings up to this point:
+    console.log(`We currently have ${warnings.count} warnings`);
+
+    // This prints all deferred warnings
+    warnings.flush();
+
+    // options is an array of "inputOptions" objects with an additional "output"
+    // property that contains an array of "outputOptions".
+    // The following will generate all outputs for all inputs, and write them to disk the same
+    // way the CLI does it:
+    for (const optionsObj of options) {
+      const bundle = await rollup.rollup(optionsObj);
+      await Promise.all(optionsObj.output.map(bundle.write));
+    }
+    console.log("Build complete");
+  });
+}
