f35e79391b6677ac1d74a900a85902a6addc62f1
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-11-28 22:43:25 +0100

    remove unused import

--- Utilities Frontend/app/rollup.config.js
+++ Utilities Frontend/app/rollup.config.js
@@ -5,7 +5,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import minifyHTML from "rollup-plugin-minify-html-literals";
 import summary from "rollup-plugin-summary";
-import serve from "rollup-plugin-serve";
 
 export default {
   plugins: [
