cac9f7011f0a6374cce02b43d9e1f1de65f6e801
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-12-06 17:31:24 +0100

    remove unused functions

--- Utilities Frontend/app/src/static-app.js
+++ Utilities Frontend/app/src/static-app.js
@@ -184,35 +184,6 @@ class StaticApp extends LitElement {
     return ["_routerChanged(routeData.page)"];
   }
 
-  // _routerChanged(page) {
-  //   alert("_routerChanged");
-  //   this.page = page || "sbf";
-  // }
-
-  // /* this pagechanged triggers for simple onserver written in page properties written above */
-  // _pageChanged(currentPage, oldPage) {
-  //   alert(currentPage);
-  //   // Firefox 1.0+
-  //   const isFirefox = typeof InstallTrigger !== "undefined";
-
-  //   switch (currentPage) {
-  //     case "bot-modeling":
-  //       if (isFirefox) {
-  //         import("./bot-modeling.js").then();
-
-  //         break;
-  //       } else {
-  //         import("./bot-modelingChrome.js").then();
-  //       }
-  //       break;
-  //     case "model-training":
-  //       import("./model-training.js").then();
-  //       break;
-  //     default:
-  //       this.page = "sbf";
-  //   }
-  // }
-
   firstUpdated() {
     const statusBar = this.shadowRoot.querySelector("#statusBar");
     statusBar.setAttribute("baseUrl", "{CONTACT_SERVICE_URL}");
