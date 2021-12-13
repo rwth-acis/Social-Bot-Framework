44015512fb2ab7d9fdef40daec98210b5b7bf9fd
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-12-13 15:40:36 +0100

    add debug widget

--- Utilities Frontend/docker/Gruntfile.js
+++ Utilities Frontend/docker/Gruntfile.js
@@ -425,6 +425,16 @@ module.exports = function (grunt) {
           "html/debug.html": ["<%= srcdir %>/widgets/widget.html.tpl"],
         },
       },
+      debug_widget_slim_html: {
+        options: {
+          data: {
+            bodyPartial: "_debug_widget_slim.tpl",
+          },
+        },
+        files: {
+          "html/debug_slim.html": ["<%= srcdir %>/widgets/widget.html.tpl"],
+        },
+      },
 
       imsld_export_widget: {
         options: {
