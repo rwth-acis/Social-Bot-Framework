b03c6f0206b17b71a601f6267319471e9bb61082
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-12-18 16:41:47 +0100

    remove unused bootstrap js script

--- Utilities Frontend/app/src/static-app.js
+++ Utilities Frontend/app/src/static-app.js
@@ -87,7 +87,6 @@ class StaticApp extends LitElement {
     }
     section .content {
       flex: 1 1 auto;
-    
     }
   `;
   constructor() {
@@ -109,11 +108,7 @@ class StaticApp extends LitElement {
           href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
           crossorigin="anonymous"
         />
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-          crossorigin="anonymous"
-        ></script>
+
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css"
