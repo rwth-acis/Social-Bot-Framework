0dc859476b8e1f269ec8ff8f9c32e95c6a4b1e5e
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-12-18 16:42:26 +0100

    remove unused script

--- Utilities Frontend/app/src/bot-modeling.js
+++ Utilities Frontend/app/src/bot-modeling.js
@@ -86,11 +86,6 @@ class BotModeling extends LitElement {
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
           crossorigin="anonymous"
         />
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-          crossorigin="anonymous"
-        ></script>
       </head>
       <div
         class="container-fluid card card-body shadow-sm mb-4"
