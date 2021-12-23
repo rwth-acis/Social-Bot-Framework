1058c85ad09257f1831f1fea63b259ddee1a1e37
lakhoune <ben.lakhoune@rwth-aachen.de>
2021-12-23 12:17:54 +0100

    bundle the openid modules

--- Utilities Frontend/app/rollup.config.js
+++ Utilities Frontend/app/rollup.config.js
@@ -7,6 +7,75 @@ import minifyHTML from "rollup-plugin-minify-html-literals";
 import summary from "rollup-plugin-summary";
 
 export default [
+  // openid callbacks
+  {
+    input: "src/callbacks/openidconnect-signin-silent-callback.js",
+    output: {
+      dir: "dist/callbacks/",
+    },
+
+    plugins: [
+      // Entry point for application build; can specify a glob to build multiple
+      // HTML files for non-SPA app
+      html({
+        input: "src/callbacks/silent-callback.html",
+      }),
+      // Resolve bare module specifiers to relative paths
+      resolve(),
+      // Minify HTML template literals
+      minifyHTML(),
+      // Minify JS
+      terser({
+        ecma: 2020,
+        module: true,
+        warnings: true,
+      }),
+    ],
+  },
+  {
+    input: "src/callbacks/openidconnect-popup-signout-callback.js",
+    output: {
+      dir: "dist/callbacks/",
+    },
+    plugins: [
+      // Entry point for application build; can specify a glob to build multiple
+      // HTML files for non-SPA app
+      html({
+        input: "src/callbacks/popup-signout-callback.html",
+      }),
+      // Resolve bare module specifiers to relative paths
+      resolve(),
+      // Minify HTML template literals
+      minifyHTML(),
+      // Minify JS
+      terser({
+        ecma: 2020,
+        module: true,
+        warnings: true,
+      }),
+    ],
+  },
+  {
+    input: "src/callbacks/openidconnect-popup-signin-callback.js",
+    output: {
+      dir: "dist/callbacks/",
+    },
+    plugins: [
+      html({
+        input: "src/callbacks/popup-signin-callback.html",
+      }),
+      // Resolve bare module specifiers to relative paths
+      resolve(),
+      // Minify HTML template literals
+      minifyHTML(),
+      // Minify JS
+      terser({
+        ecma: 2020,
+        module: true,
+        warnings: true,
+      }),
+    ],
+  },
   // main app
   {
     plugins: [
@@ -28,37 +97,12 @@ export default [
       // Print bundle summary
       summary(),
       copy({
-        targets: [
-          { src: "src/images", dest: "dist/assets" },
-          { src: "src/callbacks", dest: "dist" },
-        ],
+        targets: [{ src: "src/images", dest: "dist/assets" }],
       }),
     ],
     output: {
       dir: "dist",
     },
     preserveEntrySignatures: "strict",
-  },
-  // openid callbacks
-  {
-    input: "src/callbacks/openidconnect-signin-silent-callback.js",
-    output: {
-      file: "dist/callbacks/openidconnect-signin-silent-callback.js",
-      format: "cjs",
-    },
-  },
-  {
-    input: "src/callbacks/openidconnect-popup-signout-callback.js",
-    output: {
-      file: "dist/callbacks/openidconnect-popup-signout-callback.js",
-      format: "cjs",
-    },
-  },
-  {
-    input: "src/callbacks/openidconnect-popup-signin-callback.js",
-    output: {
-      file: "dist/callbacks/openidconnect-popup-signin-callback.js",
-      format: "cjs",
-    },
   },
 ];
