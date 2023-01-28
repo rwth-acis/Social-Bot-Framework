88c424072e7b753cb3657b3ab7ed0d1bba3cb800
lakhoune <ben.lakhoune@rwth-aachen.de>
2023-01-28 22:46:13 +0100

    update dashboard

--- Utilities Frontend/app/src/bot-modeling.js
+++ Utilities Frontend/app/src/bot-modeling.js
@@ -45,53 +45,45 @@ class BotModeling extends LitElement {
           z-index: 1;
         }
       </style>
-      <div
-        class="container-fluid  card card-body shadow-sm mb-4"
-        id="modelOpsContainer"
-      >
-        <iframe
-          id="Bot"
-          src="{SYNC_META_HOST}/syncmeta/bot.html"
-          frameborder="0"
+      <div class="container-fluid row w-100 px-0 mx-0" style="height:98vh">
+        <div class="col-9 innercontainer">
+          <div class="row h-100">
+            <div class="col-9 px-1 border-end h-100">
+              <canvas-widget></canvas-widget>
+            </div>
+            <div class="col-3  h-100">
+              <palette-widget> </palette-widget>
+            </div>
+          </div>
+        </div>
+        <div class="col-4 innercontainer ">
+          <property-browser-widget></property-browser-widget>
+        </div>
+        <div class="col-2 innercontainer" style="display:none">
+          <user-activity-widget></user-activity-widget>
+        </div>
+
+        <div
+          class="modal fade"
+          id="exportModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+          style="z-index: 2147483647 !important;"
         >
-        </iframe>
-      </div>
-      <div
-        class="container-fluid maincontainer card  border-2 shadow"
-        id="maincontainer"
-      >
-        <div class="row flex-wrap ">
-          <div class="col col-md-6">
-            <iframe
-              id="Canvas"
-              src="{SYNC_META_HOST}/syncmeta/widget.html"
-              frameborder="0"
-            >
-            </iframe>
-          </div>
-          <div class="col col-md-2 border-end border-2">
-            <iframe
-              id="Palette"
-              src="{SYNC_META_HOST}/syncmeta/palette.html"
-              frameborder="0"
-            >
-            </iframe>
-          </div>
-          <div class="col col-md-4">
-            <div class="h-100 d-flex flex-column justify-content-between px-1">
-              <iframe
-                id="Property Browser"
-                src="{SYNC_META_HOST}/syncmeta/attribute.html"
-                frameborder="0"
-              >
-              </iframe>
-              <div class="mb-3"></div>
-              <iframe
-                id="Import Tool"
-                src="{SYNC_META_HOST}/syncmeta/debug.html"
-                frameborder="0"
-              >
-              </iframe>
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">
+                <debug-widget></debug-widget>
+              </div>
             </div>
           </div>
         </div>
@@ -161,6 +153,39 @@ class BotModeling extends LitElement {
 
   createRenderRoot() {
     return this;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    setTimeout(() => {
+      const btnTemplate = `<button
+                type="button"
+                class="btn btn-secondary "
+                data-bs-toggle="modal"
+                data-bs-target="#exportModal"
+                id="exportModel"
+              >
+                <i class="bi bi-cloud-fill me-1"></i>Import/Export
+              </button>`;
+      const rowContainer = document.querySelector(
+        "#main-widget-utilities-container"
+      );
+      if (!rowContainer?.firstElementChild) {
+        console.error(
+          `Could not find the first col of row container. ${document.querySelector(
+            "#main-widget-utilities-container"
+          )} 
+          This means that the debug widget button will not be added. 
+          Make sure that the following selector is correct: #main-widget-utilities-container. The first child will be used to append the button.`
+        );
+        return;
+      }
+      rowContainer.firstElementChild?.appendChild(
+        new DOMParser().parseFromString(btnTemplate, "text/html").body
+          .firstChild 
+      );
+    }, 100);
   }
 }
 
