9efcec11368b7680a0f7ffec06fb36fb5ec3d086
lakhoune <a.b.lakhoune@gmail.com>
2023-03-02 11:06:23 +0100

    minify bundle

--- Utilities Frontend/app/vite.config.js
+++ Utilities Frontend/app/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
+import nodeResolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
+import terser from "@rollup/plugin-terser";
 import { rollupPluginHTML } from "@web/rollup-plugin-html";
+import { defineConfig } from "vite";
 import config from "./config.json";
 import packageInfo from "./package.json";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import terser from "@rollup/plugin-terser";
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
