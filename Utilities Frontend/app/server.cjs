231a62ff32a15aa3c45807cf84777e9afb459f92
lakhoune <a.b.lakhoune@gmail.com>
2023-03-03 12:28:19 +0100

    add env port

--- Utilities Frontend/app/server.cjs
+++ Utilities Frontend/app/server.cjs
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 8082;
+const port = process.env.PORT || 8082;
 
 app.use(express.static(path.join(__dirname, "dist")));
 
