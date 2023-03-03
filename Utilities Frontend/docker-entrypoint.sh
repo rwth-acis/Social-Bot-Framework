5663cfab4d5f13a1898ac47fd9cd1a8dcfd297dc
lakhoune <a.b.lakhoune@gmail.com>
2023-03-03 13:54:47 +0100

    fix error

--- Utilities Frontend/docker-entrypoint.sh
+++ Utilities Frontend/docker-entrypoint.sh
@@ -38,7 +38,7 @@ sed -i "s=<RASA_NLU>=$RASA_NLU=g" config.json
 sed -i "s=<SBF_MANAGER_HOST>=$SBF_MANAGER=g" config.json
 
 sed -i "s=localhost:1234=$YJS=g" /usr/src/app/app/node_modules/@rwth-acis/syncmeta-widgets/build/widgets/widget.container.js
-sed -i "s=yjsProtocol = \"ws\"=wss=g" /usr/src/app/app/node_modules/@rwth-acis/syncmeta-widgets/build/widgets/widget.container.js
+sed -i 's/yjsProtocol = "ws"/yjsProtocol = "wss"/g' /usr/src/app/app/node_modules/@rwth-acis/syncmeta-widgets/build/widgets/widget.container.js
 
 
 npm run build:prod
